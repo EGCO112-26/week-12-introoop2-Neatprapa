dea0de368cc588d43cab7ef7ead33991f6fc4c67
Neatprapa <nneatpream@gmail.com>
2026-03-23 09:48:57 +0000

    Assg11

--- main.cpp
+++ main.cpp
@@ -1,30 +1,57 @@
 #include <iostream>
+#include <cstdlib>
+#include "student.h"
 using namespace std;
 
 #include "student.h"
 #define N 3
 int main(int argc, char* argv[]) {
   student s1,s2;
+  //int N  = (argc-1)/2;
 
- s1.set_name("Amy"); s1.set_age(20); s2.set_name("James"); s2.set_age(17); s1.get_age()>s2.get_age() ? s1.print_name(); cout<< "is older":s2.print_name(); cout <<"is older";
+/* s1.set_name("Amy"); s1.set_age(20);
+ s2.set_name("James");
+ s2.set_age(17);
+ s1.get_age()>s2.get_age() ? s1.print_name();cout<< "is older":s2.print_name(); cout <<"is older";*/
 
 
 
   student a[N];
   // Set age and set name for the first two ppl
-  int i,age;
-  string n;
+  int i;
    // 1. Ask for N names and N age using cin
- 
+  for(i=0;i<N;i++){
+    string name = argv[(i*2)+1];
+    int age = atoi(argv[(i*2)+2]);
+
+    a[i].set_name(name);
+    a[i].set_age(age);
+  }
   
- 
+  int min_age = a[0].get_age();
+  for(i=1;i<N;i++){
+    if(a[i].get_age() < min_age){
+      min_age = a[i].get_age();
+    }
+  }
+
+  cout<<"Youngest student"<<endl;
+  for(i=0;i<N;i++){
+    if(a[i].get_age() == min_age){
+      a[i].display();
+    }
+  }
+
+
   //2. Print name and age of all N ppl
+
   
   
   
   //3. Print name and age of 
   //the youngest person
-  //Find what the age of the youngest person
+  //Find what the age of the youngest 
+  //student young=a[0];
  
   
   // Print all info for the yougest person
--- student.h
+++ student.h
@@ -1,14 +1,56 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
 #include <iostream>
+#include <string>
 using namespace std;
 
 class student{
 private:
-
   string name;
+  int age;
 
 public:
   
-   void set_name(string="Yme");
-    void print_name();
+  void set_name(string="Yme");
+  void print_name();
+  void set_age(int);
+  void display();
+  int get_age();
+
+    //1.set_age set ค่า age
+    //2.get_age return ค่า age
 
 };
+
+void student::set_name(string n){
+  name = n;
+
+}
+
+void student::print_name(){
+  cout<<"Name: "<<name<<endl;
+}
+
+void student::set_age(int x){
+  if (x > 80) { 
+        age = 80;
+    } 
+    else if (x < 5) {
+        age = 5;     
+    } 
+    else {            
+        age = x;
+    }
+
+}
+
+int student::get_age(){
+  return age;
+}
+
+void student::display(){
+  cout<<"Name: "<<name<<endl;
+  cout<<"Age: "<<age<<endl;
+}
+#endif
